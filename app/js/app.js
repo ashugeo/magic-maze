9a5f38155791ec54b81120567ee5a52975b3c1b5
Ashugeo <hugo.saintemarie@gmail.com>
2018-02-19 01:22:02 +0100

    Load multiple JSON files

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,21 @@ import sketch from './sketch'
 import Tile from './tile.js';
 
 window.tiles = [];
+window.json = [];
 
-fetch('data/tiles.json').then(response => response.json()).then(data => {
-    window.json = data;
-    new p5(sketch);
-    tiles.push(new Tile(0));
-    tiles[0].set(10, 10);
-});
+const tiles = 3;
+fetchJSON(0);
+
+function fetchJSON(i) {
+    fetch('data/tile' + i + '.json').then(response => response.json()).then(data => {
+        window.json.push(data);
+
+        if (i < tiles - 1) {
+            fetchJSON(i + 1);
+        } else {
+            new p5(sketch);
+            window.tiles.push(new Tile(0));
+            window.tiles[0].set(10, 10);
+        }
+    });
+}
