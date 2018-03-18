2c8ef5ea1a2e48b3ceba76b07108e374f87492fa
Ashugeo <hugo.saintemarie@gmail.com>
2018-03-18 03:01:08 +0100

    Remove objects keys quotes

--- app/js/config.js
+++ app/js/config.js
@@ -1,16 +1,19 @@
 export default {
-    'debug': false,
-    'grid': false,
-    'cameraSpeed': 5,
-    'boardRows': 24,
-    'boardCols': 24,
-    'size': 32,
-    'heroes': ['green', 'orange', 'purple', 'yellow'],
-    'colors': {
+    debug: false,
+    grid: false,
+    cameraSpeed: 5,
+    zoomMax: 4,
+    zoomMin: 1,
+    zoomSpeed: 15,
+    boardRows: 24,
+    boardCols: 24,
+    size: 32,
+    heroes: ['green', 'orange', 'purple', 'yellow'],
+    colors: {
         green: '#57bd6a',
         orange: '#e87b1a',
         purple: '#961c91',
         yellow: '#f7dc0a'
     },
-    'heroSpeed': 8
+    heroSpeed: 8
 }
