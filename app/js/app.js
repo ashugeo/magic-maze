a11c1b7bbcc187b8d9e535206d39a9490c3ca76e
Ashugeo <hugo.saintemarie@gmail.com>
2018-03-18 21:42:54 +0100

    Multiplayer + start game

--- app/js/app.js
+++ app/js/app.js
@@ -7,9 +7,15 @@ import pieces from './pieces';
 
 window.tiles = [];
 window.json = [];
+window.actions = {};
+window.socket = io({transports: ['websocket'], upgrade: false});
 
 const tiles = 3;
 fetchJSON(0);
+
+fetch('data/actions.json').then(response => response.json()).then(data => {
+    window.actions = data;
+});
 
 function fetchJSON(i) {
     fetch('data/tile' + i + '.json').then(response => response.json()).then(data => {
@@ -17,13 +23,38 @@ function fetchJSON(i) {
 
         if (i < tiles - 1) {
             fetchJSON(i + 1);
-        } else {
-            new p5(sketch);
-            window.tiles.push(new Tile(0));
-            window.tiles[0].set(10, 10);
         }
     });
 }
+
+function start() {
+    new p5(sketch);
+    window.tiles.push(new Tile(0));
+    window.tiles[0].set(10, 10);
+}
+
+const $players = document.getElementById('players');
+const $ui = document.getElementById('ui');
+
+socket.on('players', (players) => {
+    $players.innerHTML = players;
+    $players.innerHTML += players > 1 ? ' joueurs connectés.' : ' joueur connecté.';
+});
+
+socket.on('admin', () => {
+    $ui.innerHTML += `<div class="admin">
+        <p>Vous êtes administrateur de la partie.</p>
+        <div id="start" class="button">Commencer la partie !</div>
+    </div>`;
+    
+    document.getElementById('start').addEventListener('mousedown', () => {
+        socket.emit('start');
+    });
+});
+
+socket.on('start', () => {
+    start();
+});
 
 socket.on('hero', (data) => {
     const hero = pieces.pieces[data.id];
--- app/server.js
+++ app/server.js
@@ -9,8 +9,31 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let players = 0;
+let adminID = '';
+
 io.on('connection', (socket) => {
-    console.log('user ' + socket.id + ' connected');
+    players++;
+    io.emit('players', players);
+    console.log(players);
+
+    if (players === 1) {
+        socket.emit('admin');
+        adminID = socket.id;
+        console.log('Admin has id', adminID);
+    }
+
+    socket.on('disconnect', (socket) => {
+        players--;
+        io.emit('players', players);
+        console.log(players);
+    });
+
+    socket.on('start', () => {
+        if (socket.id === adminID) {
+            io.emit('start');
+        }
+    });
 
     socket.on('hero', (data) => {
         socket.broadcast.emit('hero', data);
