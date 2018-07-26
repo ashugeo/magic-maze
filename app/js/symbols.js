c76fcbfa6cae32c01f56efdc86bcda1deb4bd383
Ashugeo <hugo.saintemarie@gmail.com>
2018-07-27 01:43:01 +0200

    Fix grid

--- app/js/symbols.js
+++ app/js/symbols.js
@@ -38,18 +38,25 @@ export default {
     grid() {
         p5.textSize(8);
         p5.fill(0, 0, 0, 128);
-        for (let i = 0; i < config.boardCols; i += 1) {
-            // Draw rulers
+        for (let i = 0; i <= config.boardCols; i += 1) {
+            // Draw vertical lines
             p5.strokeWeight(.5);
             p5.stroke(255, 0, 0, 80);
-            p5.line(0, i*size, config.boardRows*size, i*size);
-            p5.line(i*size, 0, i*size, config.boardCols*size);
+            p5.line(i * size, 0, i * size, config.boardRows * size);
 
-            for (let j = 0; j < config.boardRows; j += 1) {
-                // Add coordinates as text
-                p5.noStroke();
-                p5.text(i + ':' + j, i*size + 6, j*size + 20);
+            if (i !== config.boardCols) {
+                for (let j = 0; j < config.boardRows; j += 1) {
+                    // Add coordinates as text
+                    p5.noStroke();
+                    p5.text(i + ':' + j, i * size + 6, j * size + 20);
+                }
             }
+        }
+
+        for (let i = 0; i <= config.boardRows; i += 1) {
+            // Draw horizontal lines
+            p5.stroke(0, 0, 255, 80);
+            p5.line(0, i * size, config.boardCols * size, i * size);
         }
     }
 }
