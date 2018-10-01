f8e18b9241b844c12102a927252dce9b2d13a764
Ashugeo <hugo.saintemarie@gmail.com>
2018-10-01 12:49:13 +0200

    Bridge with new tile ahead = explored

--- app/js/cell.js
+++ app/js/cell.js
@@ -17,13 +17,17 @@ export default class Cell {
         this.item = data.item ? data.item : false;
         this.escalator = data.escalator ? data.escalator : false;
 
-        // If bridge goes into set tile, is can be considered as explored
+        // If bridge goes into set tile, it can be considered as explored
         if (data.item && data.item.type === 'bridge') {
-            let _x = [-1, 0, 0, 1][data.tileCell.x];
-            let _y = [0, 1, -1, 0][data.tileCell.x];
-
-            let cell = board.get(this.coord.x + _x, this.coord.y + _y);
+            const _x = [-1, 0, 0, 1][data.tileCell.x];
+            const _y = [0, 1, -1, 0][data.tileCell.x];
+            const cell = board.get(this.coord.x + _x, this.coord.y + _y);
             if (!cell.isEmpty()) this.setExplored();
+
+            // Bridge goes into unexplored bridge, set it as explored as well
+            if (cell.item && cell.item.type === 'bridge') {
+                cell.setExplored();
+            }
         }
     }
 
