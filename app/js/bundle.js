b43e0d55753264a0313412cb17cb70f7088e179e
Ashugeo <hugo.saintemarie@gmail.com>
2018-12-30 23:53:35 +0100

    Remove old symbols

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -2440,36 +2440,6 @@ const size = __WEBPACK_IMPORTED_MODULE_0__config__["a" /* default */].size;
 
 /* harmony default export */ __webpack_exports__["a"] = ({
     /**
-    * Draw an arrow
-    */
-    arrow(type) {
-        if (type === 'gate') {
-            // Linear arrow
-            p5.blendMode('normal');
-            p5.line(size/2, size/4, size/2, size/1.5);
-            p5.line(size/2, size/4, size/3, size/2.5);
-            p5.line(size/2, size/4, size/1.5, size/2.5);
-            p5.blendMode('multiply');
-        } else if (type === 'enter') {
-            // Filled arrow
-            p5.strokeJoin('round');
-            p5.strokeCap('round');
-            p5.stroke(150);
-            p5.fill(255);
-            p5.beginShape();
-            p5.vertex(size/2.25, size/3);
-            p5.vertex(size/2.25, size/4);
-            p5.vertex(size/1.7, size/4);
-            p5.vertex(size/1.7, size/3);
-            p5.vertex(size/1.5, size/3);
-            p5.vertex(size/1.93, size/2);
-            p5.vertex(size/2.75, size/3);
-            p5.vertex(size/2.75, size/3);
-            p5.endShape('close');
-        }
-    },
-
-    /**
     * Draw background grid
     */
     grid() {
--- app/js/symbols.js
+++ app/js/symbols.js
@@ -3,36 +3,6 @@ const size = config.size;
 
 export default {
     /**
-    * Draw an arrow
-    */
-    arrow(type) {
-        if (type === 'gate') {
-            // Linear arrow
-            p5.blendMode('normal');
-            p5.line(size/2, size/4, size/2, size/1.5);
-            p5.line(size/2, size/4, size/3, size/2.5);
-            p5.line(size/2, size/4, size/1.5, size/2.5);
-            p5.blendMode('multiply');
-        } else if (type === 'enter') {
-            // Filled arrow
-            p5.strokeJoin('round');
-            p5.strokeCap('round');
-            p5.stroke(150);
-            p5.fill(255);
-            p5.beginShape();
-            p5.vertex(size/2.25, size/3);
-            p5.vertex(size/2.25, size/4);
-            p5.vertex(size/1.7, size/4);
-            p5.vertex(size/1.7, size/3);
-            p5.vertex(size/1.5, size/3);
-            p5.vertex(size/1.93, size/2);
-            p5.vertex(size/2.75, size/3);
-            p5.vertex(size/2.75, size/3);
-            p5.endShape('close');
-        }
-    },
-
-    /**
     * Draw background grid
     */
     grid() {
